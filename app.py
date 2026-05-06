cb529ce8052bb2beb3e7332783db93f51bca4edf
unknown <yjungade123@gmail.com>
2026-05-06 23:26:06 +0530

    port

--- app.py
+++ app.py
@@ -116,4 +116,5 @@ def upload():
 # if __name__ == "__main__":
 #     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
